bdfa011567e4e6c33efe88df747b64c563a20057
M. Emre Koral <elektronikotin@gmail.com>
2020-10-30 09:42:35 +0300

    first commit

---
+++ src/main/java/tr/com/metinemrekoral/mini_kutuphane_app/MiniKutuphaneAppApplication.java
@@ -0,0 +1,13 @@
+package tr.com.metinemrekoral.mini_kutuphane_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKutuphaneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniKutuphaneAppApplication.class, args);
+	}
+
+}
